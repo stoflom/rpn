69b17f2244c5a26d8231995cbfd3b3590a5df4e3
stoflom <stoflom@gmail.com>
2021-12-08 16:10:10 +0200

    Initial commit

---
+++ src/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+
+#include <iostream>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
+//For windows 
+#include <conio.h>
+#include <windows.h>
+
+#define	stpcpy(dest,src) ((strcpy((dest),(src)))+strlen(src))
+
+
+#define _USE_MATH_DEFINES  /* must be defined for M_PI */
+#include <math.h>
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
